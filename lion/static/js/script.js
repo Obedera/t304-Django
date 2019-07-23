961677dc5bc0e5144b99418a863c6d55489921bf
Obede <obede.silva7@gmail.com>
2019-07-23 16:37:38 -0300

    Templates

---
+++ lion/static/js/script.js
@@ -0,0 +1,1 @@
+alert('sua mae');
--- lion/views.py
+++ lion/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def pagina_inicial(request):
+    context = {
+    'nome': 'Obede', 
+    'cachorros':
+    ['mel','tobias','bob', 'costela', 'layla']}
+    return render(request, 'index.html', context)
